93023236d187bb52f93e14222f47c9ff9097e8e6
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-11-07 00:25:58 +0100

    Add main.go stub, for now just reads a PNG file

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+)
+
+var inputFile string
+
+func init() {
+	flag.StringVar(&inputFile, "input", "", "reference image")
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	flag.Parse()
+	if len(inputFile) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	fmt.Println("loading inputFile")
+	infile, err := os.Open(inputFile)
+	check(err)
+	defer infile.Close()
+
+	img, err := png.Decode(infile)
+	check(err)
+
+	check(evolve(img))
+}
+
+func evolve(img image.Image) error {
+	DNAFactory, err := NewImageDNAFactory(50, img.Bounds().Dx(), img.Bounds().Dy())
+	if err != nil {
+		return nil
+	}
+	fmt.Println(DNAFactory)
+	return nil
+}

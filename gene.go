b0a482b6a9d222201d3721f296609845f97cb963
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-11-07 09:53:43 +0100

    Add imageDNA.clone()

--- gene.go
+++ gene.go
@@ -16,3 +16,17 @@ type imageDNA struct {
 	w, h  int
 	polys []poly
 }
+
+// clone returns a new imageDNA that is an exact copy of the receiver
+func (img *imageDNA) clone() *imageDNA {
+	// copy polygon slice
+	polys := make([]poly, 0, len(img.polys))
+	for _, p := range img.polys {
+		poly := poly{col: p.col}
+		// copy points slice
+		poly.pts = make([]image.Point, 0, len(p.pts))
+		copy(poly.pts, p.pts)
+		polys = append(polys, poly)
+	}
+	return &imageDNA{polys: polys, w: img.w, h: img.h}
+}

a986204a0d6be1d4ed50026db1fe00ebef6235df
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-11-07 23:45:36 +0100

    Set up evolutionary engine

--- main.go
+++ main.go
@@ -5,7 +5,14 @@ import (
 	"fmt"
 	"image"
 	"image/png"
+	"math/rand"
 	"os"
+	"time"
+
+	"github.com/aurelien-rainone/evolve"
+	"github.com/aurelien-rainone/evolve/number"
+	"github.com/aurelien-rainone/evolve/selection"
+	"github.com/aurelien-rainone/evolve/termination"
 )
 
 var inputFile string
@@ -33,16 +40,40 @@ func main() {
 
 	img, err := png.Decode(infile)
 	check(err)
-
-	check(evolve(img))
+	check(evolveImage(img))
 }
 
-func evolve(img image.Image) error {
-	// gene factory
+func evolveImage(img image.Image) error {
+	// chromosome/image factory
 	DNAFactory, err := newImageDNAfactory(50, img.Bounds().Dx(), img.Bounds().Dy())
 	if err != nil {
 		return nil
 	}
 	fmt.Println(DNAFactory)
+
+	// mutation
+	mutationOptions := mutationOptions{}
+	mutationOptions.addPolygonMutation = number.NewConstantProbabilityGenerator(0.1)
+	mutationOptions.removePolygonMutation = number.NewConstantProbabilityGenerator(0.1)
+	mutationOptions.swapPolygonsMutation = number.NewConstantProbabilityGenerator(0.1)
+	mutation, err := newImageDNAMutation(mutationOptions)
+	if err != nil {
+		return err
+	}
+
+	// pseudo random number generator
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// define a selection strategy
+	var selectionStrategy = &selection.RouletteWheelSelection{}
+
+	engine := evolve.NewGenerationalEvolutionEngine(DNAFactory,
+		mutation,
+		fitnessEvaluator,
+		selectionStrategy,
+		rng)
+
+	result := engine.Evolve(10, 5, termination.NewTargetFitness(0, false))
+	fmt.Println(result)
 	return nil
 }

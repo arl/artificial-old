06114bef8e4e4827ea9a75ff6330c749102ef2dc
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-11-09 22:37:22 +0100

    Fix bug in imageDNA clone

--- chromosome.go
+++ chromosome.go
@@ -23,13 +23,13 @@ type imageDNA struct {
 // clone returns a new imageDNA that is an exact copy of the receiver
 func (img *imageDNA) clone() *imageDNA {
 	// copy polygon slice
-	polys := make([]poly, 0, len(img.polys))
-	for _, p := range img.polys {
+	polys := make([]poly, len(img.polys))
+	for i, p := range img.polys {
 		poly := poly{col: p.col}
 		// copy points slice
-		poly.pts = make([]image.Point, 0, len(p.pts))
+		poly.pts = make([]image.Point, len(p.pts))
 		copy(poly.pts, p.pts)
-		polys = append(polys, poly)
+		polys[i] = poly
 	}
 	return &imageDNA{polys: polys, w: img.w, h: img.h}
 }

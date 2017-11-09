6929d2514bd9b72d71eac896a77f4ff8e6f7307c
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-11-09 22:39:57 +0100

    Evaluate fitness with brute-force image diff

--- evaluation.go
+++ evaluation.go
@@ -7,16 +7,35 @@ import (
 )
 
 type fitnessEvaluator struct {
-	img image.Image // reference image
+	img *image.RGBA // reference image
+}
+
+// compare a reference image to a test image and returns the difference
+func imageDiff(ref, img *image.RGBA) float64 {
+	b := ref.Bounds()
+	w, h := b.Dx(), b.Dy()
+	// create a data set for storing all diff errors
+	data := make([]float64, w*h)
+	var offset int
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			offset = y*ref.Stride + x*4
+			r := ref.Pix[offset+0] - img.Pix[offset+0]
+			g := ref.Pix[offset+1] - img.Pix[offset+1]
+			b := ref.Pix[offset+2] - img.Pix[offset+2]
+			a := ref.Pix[offset+3] - img.Pix[offset+3]
+			data[y*w+x] = float64(r + g + b + a)
+		}
+	}
+	ds := framework.NewDataSet(framework.WithPrePopulatedDataSet(data))
+	return ds.Variance()
 }
 
 func (fe *fitnessEvaluator) Fitness(c framework.Candidate, pop []framework.Candidate) float64 {
-	//img := c.(*imageDNA)
-	img := c.(*imageDNA)
-	img.render()
-	return 0
+	return imageDiff(fe.img, c.(*imageDNA).render())
 }
 
 func (fe *fitnessEvaluator) IsNatural() bool {
-	return true
+	// the lesser the fitness the better
+	return false
 }

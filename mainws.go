8244daee92eea0f04b794ddadb32b8e80c518386
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-11-11 18:45:02 +0100

    Add websocket server stub

---
+++ mainws.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool) // connected clients
+var broadcast = make(chan Message)           // broadcast channel
+
+// Configure the upgrader
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// Define our message object
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+type GenerationMessage struct {
+	Number  int     `json:"number"`
+	Fitness float64 `json:"fitness"`
+}
+
+func main() {
+
+	// Configure websocket route
+	http.HandleFunc("/ws", handleConnections)
+
+	// Start the server on localhost port 8000 and log any errors
+	log.Println("http server started on :8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("handleConnections")
+
+	// Upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Make sure we close the connection when the function returns
+	defer ws.Close()
+
+	// Register our new client
+	clients[ws] = true
+
+	ws.WriteJSON(GenerationMessage{Number: 0, Fitness: 4135.789})
+
+	for {
+		var msg Message
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		fmt.Println("received msg:", msg)
+		// Send the newly received message to the broadcast channel
+		broadcast <- msg
+	}
+}
